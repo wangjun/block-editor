2e279b13d384b73b639a50b11760e1e2f4f88047
Rick Blalock <rblalock@pinpoint.com>
2020-09-04 13:37:42 -0400

    Updates

--- precache-manifest.a236d9a460b54d5f7711fc0497437c2e.js
+++ precache-manifest.a236d9a460b54d5f7711fc0497437c2e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "25a8d5cd60925501720447a5abdf6dbc",
+    "revision": "9b0553040aff27087d7dc2e38bcba434",
     "url": "./index.html"
   },
   {
-    "revision": "b0809e632bc83b1a5b76",
+    "revision": "65ad51e61c1eb23840db",
     "url": "./static/css/main.266c7a04.chunk.css"
   },
   {
-    "revision": "148f68f8cda108b8aea2",
-    "url": "./static/js/2.f0081668.chunk.js"
+    "revision": "039726327ae87d5fdd9d",
+    "url": "./static/js/2.7ce2d0cd.chunk.js"
   },
   {
     "revision": "810059917e9dff004fb15d8e53563a72",
-    "url": "./static/js/2.f0081668.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.7ce2d0cd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b0809e632bc83b1a5b76",
-    "url": "./static/js/main.15a328ec.chunk.js"
+    "revision": "65ad51e61c1eb23840db",
+    "url": "./static/js/main.f2ff4f87.chunk.js"
   },
   {
     "revision": "025345c4cd1ffb065f06",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c8a68052b55ba2160fc866dbf02f82b3.js"
+  "./precache-manifest.a236d9a460b54d5f7711fc0497437c2e.js"
 );
 
 self.addEventListener('message', (event) => {
